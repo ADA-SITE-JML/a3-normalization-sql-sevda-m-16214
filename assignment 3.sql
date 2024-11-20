3b6bfd3b6a6487b5df40fbd84729e09a19cff1a4
sevda-m-16214 <118723080+sevda-m-16214@users.noreply.github.com>
2024-11-20 09:33:21 +0400

    Created a table

---
+++ assignment 3.sql
@@ -0,0 +1,12 @@
+create table Literature(
+	CRN INTEGER,
+	ISBN BIGINT,
+	Title VARCHAR(255),
+	Authors VARCHAR(255), 
+	Edition INTEGER,
+	Publisher VARCHAR(255),
+	Publisher_address VARCHAR(255),
+	Pages INTEGER,
+	Publication_Year INTEGER,
+	Course_name VARCHAR(255)
+)
