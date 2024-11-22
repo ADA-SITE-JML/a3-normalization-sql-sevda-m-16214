101748d659ebdfa49bbf63df52d03fc32a683569
sevda-m-16214 <118723080+sevda-m-16214@users.noreply.github.com>
2024-11-22 18:27:50 +0400

    First Normal Form
    Renamed main table, created and updated table for Authors List entity, reached 1st normal form

--- assignment 3.sql
+++ assignment 3.sql
@@ -1,12 +1,43 @@
-create table Literature(
+CREATE TABLE courses(
 	CRN INTEGER,
 	ISBN BIGINT,
 	Title VARCHAR(255),
 	Authors VARCHAR(255), 
 	Edition INTEGER,
 	Publisher VARCHAR(255),
-	Publisher_address VARCHAR(255),
+	Publisheraddress VARCHAR(255),
 	Pages INTEGER,
-	Publication_Year INTEGER,
-	Course_name VARCHAR(255)
+	PublicationYear INTEGER,
+	Coursename VARCHAR(255)
 )
+
+CREATE TABLE AuthorsList(
+	author_id SERIAL PRIMARY KEY,
+	tem VARCHAR(255),
+	ISBN BIGINT
+)
+
+INSERT INTO AuthorsList (tem, ISBN)
+SELECT
+	TRIM(unnested_author) AS tem, isbn
+FROM(
+	SELECT
+		ISBN,
+		unnest(string_to_array(Authors, ','))
+AS unnested_author
+	FROM courses
+) subquery
+
+ALTER TABLE AuthorsList 
+	ADD COLUMN Author_Surname VARCHAR(255),
+	ADD COLUMN Author_Name VARCHAR (255)
+
+UPDATE AuthorsList
+SET 
+	Author_Name = TRIM(SPLIT_PART(tem, ' ',1)),
+	Author_Surname = TRIM(SPLIT_PART(tem,' ',2))
+
+DELETE FROM AuthorsList 
+WHERE author_id IN (10,11)
+
+ALTER TABLE AuthorsList DROP COLUMN tem
