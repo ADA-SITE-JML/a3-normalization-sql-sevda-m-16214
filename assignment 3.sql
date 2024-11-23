4e998afd5b38e681cec5843c3eebd8867f31fc18
sevda-m-16214 <118723080+sevda-m-16214@users.noreply.github.com>
2024-11-23 20:45:50 +0400

    added code for third normal form and procedure
    transformed code to work as a procedure
    renamed tables where necessary 
    found a way to copy data without using visual interface 
    created entities needed to reach third normal form

--- assignment 3.sql
+++ assignment 3.sql
@@ -1,4 +1,8 @@
-CREATE TABLE courses(
+CREATE OR REPLACE PROCEDURE assignmnet3()
+LANGUAGE plpgsql
+AS $$
+BEGIN
+	CREATE TABLE unnormal(
 	CRN INTEGER,
 	ISBN BIGINT,
 	Title VARCHAR(255),
@@ -9,63 +13,96 @@ CREATE TABLE courses(
 	Pages INTEGER,
 	PublicationYear INTEGER,
 	Coursename VARCHAR(255)
-)
+    );
 
-CREATE TABLE AuthorsList(
-	author_id SERIAL PRIMARY KEY,
-	tem VARCHAR(255),
-	ISBN BIGINT
-)
+	--import unnormalized data in csv format with UTF-8 encription
+	COPY unnormal(CRN, ISBN, Title, Authors, Edition, Publisher, Publisheraddress, Pages, PublicationYear, Coursename)
+	FROM 'E:\Unnormalized1.csv'
+	DELIMITER ','
+	CSV HEADER;
+	
+	--primary key added after importing data from csv file 
+	ALTER TABLE unnormal ADD COLUMN material_id SERIAL;
+	ALTER TABLE unnormal ADD CONSTRAINT pk_textbooks PRIMARY KEY(material_id);
 
-INSERT INTO AuthorsList (tem, ISBN)
-SELECT
-	TRIM(unnested_author) AS tem, isbn
-FROM(
+	--first normal form
+	CREATE TABLE AuthorsList(
+		author_id SERIAL PRIMARY KEY,
+		tem VARCHAR(255), --temporary column to copy into unatomized author names from data  
+		Author_Surname VARCHAR(255),
+		Author_Name VARCHAR (255),
+		ISBN BIGINT
+	);
+
+	
+	--subquery to make values in authors column atomic
+	INSERT INTO AuthorsList (tem, ISBN)
 	SELECT
-		ISBN,
-		unnest(string_to_array(Authors, ','))
-AS unnested_author
-	FROM courses
-) subquery
+		TRIM(unnested_author) AS tem, isbn
+	FROM(
+		SELECT
+			ISBN,
+			unnest(string_to_array(Authors, ','))
+	AS unnested_author
+		FROM unnormal
+	) subquery;
 
-ALTER TABLE AuthorsList 
-	ADD COLUMN Author_Surname VARCHAR(255),
-	ADD COLUMN Author_Name VARCHAR (255)
+	--seprates names from surname
+	UPDATE AuthorsList
+	SET 
+		Author_Name = TRIM(SPLIT_PART(tem, ' ',1)),
+		Author_Surname = TRIM(SPLIT_PART(tem,' ',2));
 
-UPDATE AuthorsList
-SET 
-	Author_Name = TRIM(SPLIT_PART(tem, ' ',1)),
-	Author_Surname = TRIM(SPLIT_PART(tem,' ',2))
+	--manually deleted single repeatition
+	DELETE FROM AuthorsList 
+	WHERE author_id IN (10,11);
+	
+	ALTER TABLE AuthorsList DROP COLUMN tem;
 
-DELETE FROM AuthorsList 
-WHERE author_id IN (10,11)
+	--second normal form
+	CREATE TABLE Courses(
+		CRN INTEGER, 
+		Coursename VARCHAR(255)
+	);
+	
+	INSERT INTO Courses(CRN, Coursename)
+	SELECT DISTINCT CRN, Coursename
+	FROM unnormal;
 
-ALTER TABLE AuthorsList DROP COLUMN tem
+	--relational table to illustrate many-to-many relationship between entites 
+	CREATE TABLE Course_Material(
+		CRN INTEGER,
+		ISBN BIGINT
+	);
+	
+	INSERT INTO Course_Material(CRN, ISBN)
+	SELECT DISTINCT CRN, ISBN
+	FROM unnormal;
+	
+	--third normal form
+	CREATE TABLE PublishingHouse(
+		Publisher VARCHAR(255),
+		Publisheraddress VARCHAR(255),
+		Pages INTEGER,
+		PublicationYear INTEGER,
+		ISBN BIGINT
+	);
 
+	INSERT INTO PublishingHouse(Publisher, Publisheraddress, Pages,PublicationYear,ISBN)
+	SELECT DISTINCT Publisher,Publisheraddress,Pages,PublicationYear,ISBN
+	FROM unnormal;
+	
+	ALTER TABLE PublishingHouse ADD CONSTRAINT pk_pudl PRIMARY KEY(ISBN);
+	
+	CREATE TABLE Textbooks(
+		ISBN BIGINT,
+		Title VARCHAR(255)
+	);
+	
+	INSERT INTO Textbooks(ISBN, Title)
+	SELECT DISTINCT ISBN, Title
+	FROM unnormal;
+END;
+$$;
 
-ALTER TABLE courses RENAME TO Textbooks
-ALTER TABLE Textbooks ADD COLUMN material_id SERIAL
-ALTER TABLE Textbooks ADD CONSTRAINT pk_textbooks PRIMARY KEY(material_id)
-
-CREATE TABLE Courses(
-	CRN INTEGER PRIMARY KEY,
-	Coursename VARCHAR(255)
-)
-
-INSERT INTO Courses(CRN, Coursename)
-SELECT DISTINCT CRN, Coursename
-FROM Textbooks
-
-
-CREATE TABLE Course_textbooks(
-	CRN INTEGER,
-	ISBN BIGINT
-)
-
-INSERT INTO Course_textbooks(CRN, ISBN)
-SELECT DISTINCT CRN, ISBN
-FROM Textbooks
-
-
-
-
+CALL assignmnet3();
